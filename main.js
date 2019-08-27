15abfc06e8a25b2047b5cc7cfc989e3b430f12f6
Brian Wood <46594157+BrianMJWood@users.noreply.github.com>
2019-08-27 10:31:49 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        users: [],
+        nearbyUsers: [],
+        displayUser: [],
+        displaySearch: false,
+        displayMatches: true,
+        displayMessages: false,
+        displayProfile: false
+    },
+    methods: {
+        getData() {
+            var data;
+            fetch("data.json", {
+                method: "GET",
+            }).then(function (response) {
+
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error(response.statusText);
+            }).then(function (json) {
+                data = json;
+
+                app.users = data.users;
+
+// Once user profile data is loaded, have this check against                
+
+                for (i = 0; i < app.users.length; i++) {
+                    if (app.users[i].city == "Barcelona") {
+                        app.nearbyUsers.push(app.users[i])
+                    };
+                }
+                app.displayUser = app.nearbyUsers[Math.floor(Math.random() * app.nearbyUsers.length)]
+                                
+
+            }).catch(function (error) {
+                console.log("Request failed: " + error.message);
+            });
+        },
+        searchUser() {
+            app.displayUser = app.nearbyUsers[Math.floor(Math.random() * app.nearbyUsers.length)];
+            return app.displayUser;
+        },
+
+
+    },
+    created() {
+        this.getData();
+    },
+
+});
+
+// When searchUser() runs, I want my code to look through an array of availableUsers, choose one randomly, and then output that profile.
+
+// availableUsers needs to be anyone that matches my city/location.
+
+// Now I've got available users. I want to randomly pick one of them to display. 
